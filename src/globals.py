2c6a4a8a409224b37508b0f49455e4bd6b075443
Samuel Castan <samuel.castan96@gmail.com>
2023-10-20 15:50:57 -0600

    more changes

---
+++ src/globals.py
@@ -0,0 +1,3 @@
+CAT_FEATURES = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
+
+NUM_FEATURES = ["age", "capital-gain", "capital-loss", "hours-per-week"]

73c378a70b49c01daa47a1294bbb9885c99407de
Samuel Castan <samuel.castan96@gmail.com>
2023-11-08 19:05:17 -0600

    model development in jupyter notebook completed

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -12,9 +12,13 @@ df = pd.read_csv("data/raw/census.csv")
 
 df.columns = [column.replace(" ", "") for column in df.columns]
 
-cat_features = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
+columns = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country", "salary"]
 
-for feature in cat_features:
-    df[feature] = df[feature].str.replace(" ", "")
+for column in columns:
+    df[column] = df[column].str.replace(" ", "")
 
+# Remove duplicates
+df = df.drop_duplicates()
+
+# Export cleaned dataset
 df.to_csv("data/clean/census.csv", index=False)

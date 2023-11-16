21a411cd3bc35d8282d696e2d3b32fecd44900ba
Samuel Castan <samuel.castan96@gmail.com>
2023-11-16 14:12:14 -0600

    advancement

--- src/globals.py
+++ src/globals.py
@@ -1,3 +1,5 @@
 CAT_FEATURES = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
 
 NUM_FEATURES = ["age", "capital-gain", "capital-loss", "hours-per-week"]
+
+TARGET = "salary"
--- src/model_training.py
+++ src/model_training.py
@@ -1,14 +1,34 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+import globals
+
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
-
+# Read data
 df = pd.read_csv('data/clean/census.csv')
 
-X
-# X_train, X_test, y_train, y_test = train_test_split()
+# Features and target variables
+X = df[globals.CAT_FEATURES + globals.NUM_FEATURES]
+y = df[globals.TARGET]
 
+# First split to obtain testing dastaset and temporal set for balancing the classes
+X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-model = RandomForestClassifier()
+df_temp = pd.concat([X_temp, y_temp], axis=1)
 
+del X_temp, y_temp
 
+undersampling_size = len(df_temp[df_temp["salary"]==">50K"])
+
+df_temp = pd.concat(
+    [df_temp[df_temp["salary"] == "<=50K"].sample(undersampling_size), 
+     df_temp[df_temp["salary"] == ">50K"]]
+)
+
+X_train = df_temp[globals.CAT_FEATURES + globals.NUM_FEATURES]
+y_train = df_temp[globals.TARGET]

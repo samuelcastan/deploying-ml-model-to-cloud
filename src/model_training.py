bb6d6a2fbe8a9d6288735eff63aa2669c0c7334a
Samuel Castan <samuel.castan96@gmail.com>
2023-11-16 14:21:07 -0600

    model training finished

--- src/model_training.py
+++ src/model_training.py
@@ -1,12 +1,11 @@
 import pandas as pd
+import joblib
 import globals
 
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 
 # Read data
@@ -19,6 +18,7 @@ y = df[globals.TARGET]
 # First split to obtain testing dastaset and temporal set for balancing the classes
 X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+# Start the imbalanced dataset
 df_temp = pd.concat([X_temp, y_temp], axis=1)
 
 del X_temp, y_temp
@@ -32,3 +32,36 @@ df_temp = pd.concat(
 
 X_train = df_temp[globals.CAT_FEATURES + globals.NUM_FEATURES]
 y_train = df_temp[globals.TARGET]
+
+
+
+column_transformer = ColumnTransformer(
+    transformers=[
+        # name, transformer, columns
+        ('cat', OneHotEncoder(handle_unknown="ignore"), globals.CAT_FEATURES),
+    ],
+        # Ignore numerical columns
+        remainder='passthrough'
+)
+
+classifier = RandomForestClassifier(
+    n_estimators=100,
+    n_jobs=-1,
+    max_depth=15,
+    max_features="sqrt",
+    random_state=42
+    )
+
+
+pipeline = Pipeline(
+    steps=[
+        ('one-hot-encoder', column_transformer),
+        ('classifier', classifier)
+    ]
+)
+
+pipeline.fit(X_train, y_train)
+
+
+# Save model
+joblib.dump(pipeline, "model/inference_pipeline.pkl")

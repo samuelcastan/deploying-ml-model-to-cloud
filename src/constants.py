96fa1fcb9826b38f64607ed97e868926901dc9f2
Samuel Castan <samuel.castan96@gmail.com>
2023-12-01 17:13:02 -0600

    finishing model_training.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,20 @@
+'''''
+Constants used in the project
+
+Author: Samuel Castan
+Date: Nov 2023
+'''
+
+
 CAT_FEATURES = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
 
 NUM_FEATURES = ["age", "capital-gain", "capital-loss", "hours-per-week"]
 
 TARGET = "salary"
+
+TRAINING_DATASET = 'data/clean/census.csv'
+
+TEST_SIZE = 0.3
+
+RANDOM_STATE = 42
+
--- src/model_training.py
+++ src/model_training.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-import globals
+import constants
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -8,60 +8,94 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 
-# Read data
-df = pd.read_csv('data/clean/census.csv')
 
-# Features and target variables
-X = df[globals.CAT_FEATURES + globals.NUM_FEATURES]
-y = df[globals.TARGET]
+def balance_dataset(X, y, test_size, random_state):
+    ''''
+    Receives an imbalanced dataset and returns features and target variable with same proportions downsampling the mayority class
 
-# First split to obtain testing dastaset and temporal set for balancing the classes
-X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    Inputs:
+        X: Independent variables
+        y: Target or dependendent variable
+    '''
 
-# Start the imbalanced dataset
-df_temp = pd.concat([X_temp, y_temp], axis=1)
+    # First split to obtain testing dastaset and temporal set for balancing the classes
+    X_temp, X_test, y_temp, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state)
 
-del X_temp, y_temp
+    # Start the imbalanced dataset
+    df_temp = pd.concat([X_temp, y_temp], axis=1)
 
-undersampling_size = len(df_temp[df_temp["salary"]==">50K"])
+    del X_temp, y_temp
 
-df_temp = pd.concat(
-    [df_temp[df_temp["salary"] == "<=50K"].sample(undersampling_size), 
-     df_temp[df_temp["salary"] == ">50K"]]
-)
+    undersampling_size = len(df_temp[df_temp[constants.TARGET] == ">50K"])
 
-X_train = df_temp[globals.CAT_FEATURES + globals.NUM_FEATURES]
-y_train = df_temp[globals.TARGET]
-
-
-
-column_transformer = ColumnTransformer(
-    transformers=[
-        # name, transformer, columns
-        ('cat', OneHotEncoder(handle_unknown="ignore"), globals.CAT_FEATURES),
-    ],
-        # Ignore numerical columns
-        remainder='passthrough'
-)
-
-classifier = RandomForestClassifier(
-    n_estimators=100,
-    n_jobs=-1,
-    max_depth=15,
-    max_features="sqrt",
-    random_state=42
+    df_temp = pd.concat(
+        [df_temp[df_temp[constants.TARGET] == "<=50K"].sample(undersampling_size),
+         df_temp[df_temp[constants.TARGET] == ">50K"]]
     )
 
+    X_train = df_temp[constants.CAT_FEATURES + constants.NUM_FEATURES]
+    y_train = df_temp[constants.TARGET]
 
-pipeline = Pipeline(
-    steps=[
-        ('one-hot-encoder', column_transformer),
-        ('classifier', classifier)
-    ]
-)
-
-pipeline.fit(X_train, y_train)
+    return X_train, X_test, y_train, y_test
 
 
-# Save model
-joblib.dump(pipeline, "model/inference_pipeline.pkl")
+def make_pipeline(X_train, y_train):
+    '''
+    Trains the the entire ML inference pipeline: should train on the provided data. 
+
+    Inputs:
+        data: Dataframe containing features and target variables
+        cat_features: List of categorical features 
+        num_features: List of numerical features
+        target: Target Variables
+
+    Output:
+        inference_pipeline: Pipeline that one-hot encodes categorical features and trains a Random Forest Model for later saving
+    '''
+
+    
+    column_transformer = ColumnTransformer(
+        transformers=[
+            # name, transformer, columns
+            ('cat', OneHotEncoder(handle_unknown="ignore"), constants.CAT_FEATURES),
+        ],
+        # Ignore numerical columns
+        remainder='passthrough'
+    )
+
+    classifier = RandomForestClassifier(
+        n_estimators=100,
+        n_jobs=-1,
+        max_depth=15,
+        max_features="sqrt",
+        random_state=42
+    )
+
+    pipeline = Pipeline(
+        steps=[
+            ('one-hot-encoder', column_transformer),
+            ('classifier', classifier)
+        ]
+    )
+
+    pipeline.fit(X_train, y_train)
+
+    return pipeline
+
+
+if __name__ == '__main__':
+    # Read data
+    df = pd.read_csv(constants.TRAINING_DATASET)
+
+    # Features and target variables
+    X = df[constants.CAT_FEATURES + constants.NUM_FEATURES]
+    y = df[constants.TARGET]
+
+    X_train, X_test, y_train, y_test = balance_dataset(
+        X, y, test_size=constants.TEST_SIZE, random_state=constants.RANDOM_STATE)
+    
+    pipeline = make_pipeline(X_train=X_train, y_train=y_train)
+
+    # Save pipeline
+    joblib.dump(pipeline, "model/inference_pipeline.pkl")
